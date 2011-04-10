dba184170d056e14a62273de48aa681bb31ef1b1
Hüseyin Kozan <posta@huseyinkozan.com.tr>
2011-04-10 17:20:08 +0300

    Added translation files.

---
+++ pisido_tr.ts
@@ -0,0 +1,241 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="tr_TR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>PiSiDo</source>
+        <translation>PiSiDo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>Source Code</source>
+        <translation>Kaynak Kod</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="34"/>
+        <source>Source</source>
+        <translation>Kaynak</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="42"/>
+        <source>Compressed File</source>
+        <translation>Sıkıştırılmış Dosya</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <location filename="mainwindow.ui" line="90"/>
+        <location filename="mainwindow.ui" line="217"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>Folder Path</source>
+        <translation>Dizin Yolu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>Copy source code to pisi work directory</source>
+        <translation>Kaynak kodu, pisi çalışma dizinine kopyala</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <source>Copy sorce code to package work directory</source>
+        <translation>Kaynak kodu, paket çalışma dizinine kopyala</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="130"/>
+        <source>Package</source>
+        <translation>Paket</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="163"/>
+        <location filename="mainwindow.ui" line="735"/>
+        <source>Name</source>
+        <translation>İsim</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="173"/>
+        <source>Work Dir</source>
+        <translation>Çalışma Dizini</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="187"/>
+        <source>Files</source>
+        <translation>Dosyalar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="224"/>
+        <source>Version</source>
+        <translation>Sürüm</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="234"/>
+        <source>Add</source>
+        <translation>Ekle</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="241"/>
+        <source>Remove</source>
+        <translation>Çıkar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <source>Clear</source>
+        <translation>Temizle</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="255"/>
+        <source>Licence</source>
+        <translation>Lisans</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="262"/>
+        <source>Type</source>
+        <translation>Tip</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="275"/>
+        <source>Brief Description</source>
+        <translation>Kısa Açıklama</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="285"/>
+        <source>Detailed Description</source>
+        <translation>Ayrıntılı Açıklama</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="346"/>
+        <source>Other</source>
+        <translation>Diğer</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="630"/>
+        <source>Compilation</source>
+        <translation>Derleme</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="636"/>
+        <source>Action Modules</source>
+        <translation>Action Modülleri</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="687"/>
+        <source>Create .desktop file</source>
+        <translation>.desktop dosyası oluştur</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="694"/>
+        <source>Create &lt;icon&gt;&lt;/icon&gt; tag</source>
+        <translation>&lt;icon&gt;&lt;/icon&gt; etiketi oluştur</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="701"/>
+        <source>Create service file</source>
+        <translation>servis dosyası oluştur</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="708"/>
+        <source>Create COMAR file</source>
+        <translation>COMAR dosyası oluştur</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="729"/>
+        <source>Packager</source>
+        <translation>Paketleyici</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="748"/>
+        <source>E-Mail</source>
+        <translation>E-Posta</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="761"/>
+        <location filename="mainwindow.ui" line="764"/>
+        <source>Clears and resets all options</source>
+        <translation>Bütün seçenekleri temizler ve ilk haline getirir</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="767"/>
+        <source>&amp;Clear</source>
+        <translation>&amp;Temizle</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="787"/>
+        <location filename="mainwindow.ui" line="790"/>
+        <source>Creates files to make pisi package. All necessary fields must filled before use.</source>
+        <translation>Pisi paketi yapmak için gereken dosyaları oluşturur. Gerekli bütün alanlar doldurulmalıdır.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="793"/>
+        <source>C&amp;reate</source>
+        <translation>&amp;Oluştur</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="812"/>
+        <source>&amp;File</source>
+        <translation>&amp;Dosya</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="818"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Ayarlar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="824"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Yardım</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="838"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Çıkış</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="841"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="846"/>
+        <source>&amp;Configure Application...</source>
+        <translation>Uygulamayı &amp;Yapılandır...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="851"/>
+        <source>Open &amp;Help Page...</source>
+        <translation>&amp;Yardım Sayfasını Aç...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="854"/>
+        <source>F1</source>
+        <translation>F1</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="859"/>
+        <source>&amp;About...</source>
+        <translation>&amp;Hakkında...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="864"/>
+        <source>About &amp;Qt...</source>
+        <translation>&amp;Qt Hakkında...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="20"/>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message utf8="true">
+        <location filename="mainwindow.cpp" line="20"/>
+        <source>This program written by Hüseyin Kozan.
+http://huseyinkozan.com.tr
+App Version:%1</source>
+        <translation>Bu program Hüseyin Kozan tarafından yazıldı.
+http://huseyinkozan.com.tr
+uygulama Sürümü: %1</translation>
+    </message>
+</context>
+</TS>
